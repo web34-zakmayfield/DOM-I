851650e11f766af4153a02816a2ed23f1f1e61e5
Zachary Mayfield <zakmayfield@Zacharys-MacBook-Pro.local>
2020-08-03 11:30:19 -0400

    MVP done

--- js/index.js
+++ js/index.js
@@ -48,6 +48,7 @@ let logo = document.getElementById('logo-img')
 ;``
 logo.setAttribute('src', siteContent['nav']['img-src'])
 
+
 /////NAV
 let navItems = document.querySelectorAll('a')
 
@@ -58,6 +59,7 @@ navItems[3].textContent = siteContent['nav']['nav-item-4']
 navItems[4].textContent = siteContent['nav']['nav-item-5']
 navItems[5].textContent = siteContent['nav']['nav-item-6']
 
+
 /////CTA
 let ctaH1 = document.querySelector('.cta-text h1')
 let ctaBtn = document.querySelector('.cta-text button')
@@ -66,6 +68,7 @@ let ctaImg = document.querySelector('#cta-img')
 ctaH1.textContent = siteContent["cta"]["h1"]
 ctaBtn.textContent = siteContent["cta"]["button"]
 ctaImg.src = siteContent["cta"]["img-src"]
+
 
 /////MAIN CONTENT
 //top - features/about
@@ -79,10 +82,40 @@ let aboutP = document.querySelector('.top-content .text-content:nth-child(2) p')
 aboutH4.textContent = siteContent["main-content"]["about-h4"]
 aboutP.textContent = siteContent["main-content"]["about-content"]
 
+
 //middle - img
 let middleImg = document.querySelector('#middle-img')
 middleImg.src = siteContent["main-content"]["middle-img-src"]
 
+
 //bottom - services/product/vision
+let servicesH4 = document
+  .querySelector('.bottom-content .text-content:nth-child(1) h4')
+let servicesP = document
+  .querySelector('.bottom-content .text-content:nth-child(1) p')
+servicesH4.textContent = siteContent["main-content"]["services-h4"]
+servicesP.textContent = siteContent["main-content"]["services-content"]
+
+let productH4 = document
+  .querySelector('.bottom-content .text-content:nth-child(2) h4')
+let productP = document
+  .querySelector('.bottom-content .text-content:nth-child(2) p')
+productH4.textContent = siteContent["main-content"]["product-h4"]
+productP.textContent = siteContent["main-content"]["product-content"]
+
+let visionH4 = document
+  .querySelector('.bottom-content .text-content:nth-child(3) h4')
+let visionP = document
+  .querySelector('.bottom-content .text-content:nth-child(3) p')
+visionH4.textContent = siteContent["main-content"]["vision-h4"]
+visionP.textContent = siteContent["main-content"]["vision-content"]
 
 
+/////CONTACT
+let contactH4 = document.querySelector('.contact h4')
+let contactPees = document.querySelectorAll('.contact p')
+
+contactH4.textContent = siteContent["contact"]["contact-h4"]
+contactPees[0].textContent = siteContent["contact"]["address"]
+contactPees[1].textContent = siteContent["contact"]["email"]
+contactPees[2].textContent = siteContent["contact"]["phone"]
